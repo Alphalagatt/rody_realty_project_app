4bdeeedb508dd204c47b57721c6e5aa8411eba73
ankacharya <32520909+ankacharya@users.noreply.github.com>
2024-03-07 13:22:28 +1100

    forget password

--- src/Views/Authentication_and_authorization/forget_password.js
+++ src/Views/Authentication_and_authorization/forget_password.js
@@ -1,12 +1,27 @@
+import React from "react";
 
 function ForgetPassword() {
-    return(
-        <div>
-            Your code for the forget password page goes here..
 
-            Please delete this text and add your ui code here.
-        </div>
-    )
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+    const emailVal = e.target.email.value;
+    sendPasswordResetEmail(database,emailVal).then(data=>{
+        alert("check your gamil")
+        history("/")
+    }).catch(err=>{
+        alert(err.code)
+    })
+
+    }
+return(
+<div className="App">
+    <h1>ForgetPassword</h1>
+    <form onSubmit = {(e)=>handleSubmit(e)}>
+        <input name="email"/><br/>
+        <button>Reset</button>
+    </form>
+</div>  
+ )  
 }
 
 export default ForgetPassword;
